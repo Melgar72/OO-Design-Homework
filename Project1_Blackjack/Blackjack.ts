c167d94d86c4f38938fff946b2cec83406960f83
Melgar72 <dmelgar.9.dm@gmail.com>
2026-02-08 22:32:10 -0800

    Make plans for Blackjack

---
+++ Project1_Blackjack/Blackjack.ts
@@ -0,0 +1,10 @@
+/*
+Class / Structure for player / dealer. 
+Actions: Hit, Stand. 
+Check for bust, push
+*/
+
+/*
+Class / structure for deck of cards
+Suits not req'd, but have 4 of each card
+*/
