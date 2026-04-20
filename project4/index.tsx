deaf7bd97538abdde85f08269f65ad0cdc9187f8
Melgar72 <dmelgar.9.dm@gmail.com>
2026-04-19 22:47:24 -0700

    Attempt separate file imports.

--- project4/index.tsx
+++ project4/index.tsx
@@ -1,44 +1,6 @@
 import React, { StrictMode, Component, ReactNode } from "react";
 import { createRoot } from "react-dom/client";
 
-interface MyListProps {
-    maxItems?: number;
-    children?: React.JSX.Element | React.JSX.Element[];
-}
-
-class MyListState {
-    nClicks : number = 0;
-}
-
-class MyList extends Component<MyListProps, MyListState> {
-    constructor(props: MyListProps) {
-        super(props);
-        this.state = new MyListState();
-        this.addClick = this.addClick.bind(this);
-    }
-
-    override render(): ReactNode {
-        const children = React.Children.toArray(this.props.children);
-        const result = []
-        const nChildren = this.props.maxItems ?? children.length;
-    
-        for( let child = 0; child < Math.min(nChildren, children.length); child++ ) {
-            result.push( children[child] );
-        }
-
-        result.push( <li>You have clicked {this.state.nClicks} times.</li> );
-
-        return <ul onClick={this.addClick}>{result}</ul>
-    }
-
-    addClick(): void {
-        let newState = new MyListState();
-        newState.nClicks = this.state.nClicks + 1;
-        this.setState( newState );
-    }
-}
-
-
 interface TableProps {
     maxItems?: number;
     children?: React.JSX.Element | React.JSX.Element[];
@@ -127,12 +89,6 @@ class RouletteTable extends Component {
                 <button className="square">34</button>
             </div>
             </MyTable>
-
-            <MyList maxItems={3}>
-                <li>one</li>
-                <li>two</li>
-                <li>three</li>
-            </MyList>
         </div>
         )
     }
