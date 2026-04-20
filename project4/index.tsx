17b00e7b7c2a3920ba51a89a8aea0fbf3f2f4626
Melgar72 <dmelgar.9.dm@gmail.com>
2026-04-19 22:38:46 -0700

    Modify original test code to fit new goal.

--- project4/index.tsx
+++ project4/index.tsx
@@ -1,21 +1,6 @@
 import React, { StrictMode, Component, ReactNode } from "react";
 import { createRoot } from "react-dom/client";
 
-import Table from "./Table";
-
-const rootElem = document.getElementById('root');
-
-if( rootElem == null ) {
-    alert('you forgot to put a root element in your HTML file.');
-}
-const root = createRoot(rootElem as HTMLElement);
-root.render(
-    <StrictMode>
-        <Table />
-    </StrictMode>
-);
-
-{/*
 interface MyListProps {
     maxItems?: number;
     children?: React.JSX.Element | React.JSX.Element[];
@@ -53,16 +38,103 @@ class MyList extends Component<MyListProps, MyListState> {
     }
 }
 
-class App extends Component {
+
+interface TableProps {
+    maxItems?: number;
+    children?: React.JSX.Element | React.JSX.Element[];
+}
+
+class TableState {
+    nClicks : number = 0;
+}
+
+class MyTable extends Component<TableProps, TableState> {
+    constructor(props: TableProps) {
+        super(props);
+        this.state = new TableState();
+        this.addClick = this.addClick.bind(this);
+    }
+
     override render(): ReactNode {
-        return <div>
-            <p>welcome to my web app.</p>
-            <MyList maxItems={2}>
+        const children = React.Children.toArray(this.props.children);
+        const result = []
+        const nChildren = this.props.maxItems ?? children.length;
+    
+        for( let child = 0; child < Math.min(nChildren, children.length); child++ ) {
+            result.push( children[child] );
+        }
+
+        result.push( <li>You have clicked {this.state.nClicks} times.</li> );
+
+        return <ul onClick={this.addClick}>{result}</ul>
+    }
+
+    addClick(): void {
+        let newState = new TableState();
+        newState.nClicks = this.state.nClicks + 1;
+        this.setState( newState );
+    }
+}
+
+
+class RouletteTable extends Component {
+    override render(): ReactNode {
+        return (
+        <div>
+            <p>Welcome to the Roulette table!</p>
+
+            <MyTable maxItems={36}>
+            <div className="board-row">
+                <button className="square">3</button>
+                <button className="square">6</button>
+                <button className="square">9</button>
+                <button className="square">12</button>
+                <button className="square">15</button>
+                <button className="square">18</button>
+                <button className="square">21</button>
+                <button className="square">24</button>
+                <button className="square">27</button>
+                <button className="square">30</button>
+                <button className="square">33</button>
+                <button className="square">36</button>
+            </div>
+            <div className="board-row">
+                <button className="square">2</button>
+                <button className="square">5</button>
+                <button className="square">8</button>
+                <button className="square">11</button>
+                <button className="square">14</button>
+                <button className="square">17</button>
+                <button className="square">20</button>
+                <button className="square">23</button>
+                <button className="square">26</button>
+                <button className="square">29</button>
+                <button className="square">32</button>
+                <button className="square">35</button>
+            </div>
+            <div className="board-row">
+                <button className="square">1</button>
+                <button className="square">4</button>
+                <button className="square">7</button>
+                <button className="square">10</button>
+                <button className="square">13</button>
+                <button className="square">16</button>
+                <button className="square">19</button>
+                <button className="square">22</button>
+                <button className="square">25</button>
+                <button className="square">28</button>
+                <button className="square">31</button>
+                <button className="square">34</button>
+            </div>
+            </MyTable>
+
+            <MyList maxItems={3}>
                 <li>one</li>
                 <li>two</li>
                 <li>three</li>
             </MyList>
         </div>
+        )
     }
 }
 
@@ -76,7 +148,6 @@ const root = createRoot( rootElem as HTMLElement );
 
 root.render(
     <StrictMode>
-        <App/>
+        <RouletteTable/>
     </StrictMode>
 );
-*/}
--- project4/src/index.jsx
+++ project4/src/index.jsx
@@ -22,84 +22,128 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importStar(require("react"));
 const client_1 = require("react-dom/client");
-const Table_1 = __importDefault(require("./Table"));
+class MyListState {
+    constructor() {
+        this.nClicks = 0;
+    }
+}
+class MyList extends react_1.Component {
+    constructor(props) {
+        super(props);
+        this.state = new MyListState();
+        this.addClick = this.addClick.bind(this);
+    }
+    render() {
+        var _a;
+        const children = react_1.default.Children.toArray(this.props.children);
+        const result = [];
+        const nChildren = (_a = this.props.maxItems) !== null && _a !== void 0 ? _a : children.length;
+        for (let child = 0; child < Math.min(nChildren, children.length); child++) {
+            result.push(children[child]);
+        }
+        result.push(<li>You have clicked {this.state.nClicks} times.</li>);
+        return <ul onClick={this.addClick}>{result}</ul>;
+    }
+    addClick() {
+        let newState = new MyListState();
+        newState.nClicks = this.state.nClicks + 1;
+        this.setState(newState);
+    }
+}
+class TableState {
+    constructor() {
+        this.nClicks = 0;
+    }
+}
+class MyTable extends react_1.Component {
+    constructor(props) {
+        super(props);
+        this.state = new TableState();
+        this.addClick = this.addClick.bind(this);
+    }
+    render() {
+        var _a;
+        const children = react_1.default.Children.toArray(this.props.children);
+        const result = [];
+        const nChildren = (_a = this.props.maxItems) !== null && _a !== void 0 ? _a : children.length;
+        for (let child = 0; child < Math.min(nChildren, children.length); child++) {
+            result.push(children[child]);
+        }
+        result.push(<li>You have clicked {this.state.nClicks} times.</li>);
+        return <ul onClick={this.addClick}>{result}</ul>;
+    }
+    addClick() {
+        let newState = new TableState();
+        newState.nClicks = this.state.nClicks + 1;
+        this.setState(newState);
+    }
+}
+class RouletteTable extends react_1.Component {
+    render() {
+        return (<div>
+            <p>Welcome to the Roulette table!</p>
+
+            <MyTable maxItems={36}>
+            <div className="board-row">
+                <button className="square">3</button>
+                <button className="square">6</button>
+                <button className="square">9</button>
+                <button className="square">12</button>
+                <button className="square">15</button>
+                <button className="square">18</button>
+                <button className="square">21</button>
+                <button className="square">24</button>
+                <button className="square">27</button>
+                <button className="square">30</button>
+                <button className="square">33</button>
+                <button className="square">36</button>
+            </div>
+            <div className="board-row">
+                <button className="square">2</button>
+                <button className="square">5</button>
+                <button className="square">8</button>
+                <button className="square">11</button>
+                <button className="square">14</button>
+                <button className="square">17</button>
+                <button className="square">20</button>
+                <button className="square">23</button>
+                <button className="square">26</button>
+                <button className="square">29</button>
+                <button className="square">32</button>
+                <button className="square">35</button>
+            </div>
+            <div className="board-row">
+                <button className="square">1</button>
+                <button className="square">4</button>
+                <button className="square">7</button>
+                <button className="square">10</button>
+                <button className="square">13</button>
+                <button className="square">16</button>
+                <button className="square">19</button>
+                <button className="square">22</button>
+                <button className="square">25</button>
+                <button className="square">28</button>
+                <button className="square">31</button>
+                <button className="square">34</button>
+            </div>
+            </MyTable>
+
+            <MyList maxItems={3}>
+                <li>one</li>
+                <li>two</li>
+                <li>three</li>
+            </MyList>
+        </div>);
+    }
+}
 const rootElem = document.getElementById('root');
 if (rootElem == null) {
     alert('you forgot to put a root element in your HTML file.');
 }
 const root = (0, client_1.createRoot)(rootElem);
 root.render(<react_1.StrictMode>
-        <Table_1.default />
+        <RouletteTable />
     </react_1.StrictMode>);
-{ /*
-interface MyListProps {
-    maxItems?: number;
-    children?: React.JSX.Element | React.JSX.Element[];
-}
-
-class MyListState {
-    nClicks : number = 0;
-}
-
-class MyList extends Component<MyListProps, MyListState> {
-    constructor(props: MyListProps) {
-        super(props);
-        this.state = new MyListState();
-        this.addClick = this.addClick.bind(this);
-    }
-
-    override render(): ReactNode {
-        const children = React.Children.toArray(this.props.children);
-        const result = []
-        const nChildren = this.props.maxItems ?? children.length;
-    
-        for( let child = 0; child < Math.min(nChildren, children.length); child++ ) {
-            result.push( children[child] );
-        }
-
-        result.push( <li>You have clicked {this.state.nClicks} times.</li> );
-
-        return <ul onClick={this.addClick}>{result}</ul>
-    }
-
-    addClick(): void {
-        let newState = new MyListState();
-        newState.nClicks = this.state.nClicks + 1;
-        this.setState( newState );
-    }
-}
-
-class App extends Component {
-    override render(): ReactNode {
-        return <div>
-            <p>welcome to my web app.</p>
-            <MyList maxItems={2}>
-                <li>one</li>
-                <li>two</li>
-                <li>three</li>
-            </MyList>
-        </div>
-    }
-}
-
-const rootElem = document.getElementById('root');
-
-if( rootElem == null ) {
-    alert('you forgot to put a root element in your HTML file.');
-}
-
-const root = createRoot( rootElem as HTMLElement );
-
-root.render(
-    <StrictMode>
-        <App/>
-    </StrictMode>
-);
-*/
-}
